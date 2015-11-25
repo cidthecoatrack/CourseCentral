4c5d05fba839acb334e8183b16189d8ad127b18d
Karl Speer <cidthecoatrack@gmail.com>
2015-11-25 18:55:50 -0500

    More tests
    but dinner time

--- Database/SampleData.sql
+++ Database/SampleData.sql
@@ -7,25 +7,25 @@ INSERT INTO Students (Id, FirstName, LastName, DateOfBirth)
 VALUES ('E3FD0B4E-F681-4074-800F-D22838F6FFA9', 'Ferris', 'Bueller', '1985-06-09')
 
 INSERT INTO Courses (Id, Name, Department, Number, Section, Professor, Year, Semester)
-VALUES ('0A127D6F-913F-49AA-AABB-1E712E5939C5', 'Database Construction and Maintenance', 'CSCI', '2669', 'A', 'Robert Martin', 2015, 'FALL')
+VALUES ('0A127D6F-913F-49AA-AABB-1E712E5939C5', 'Database Construction and Maintenance', 'CSCI', 2669, 'A', 'Robert Martin', 2015, 'FALL')
 
 INSERT INTO Courses (Id, Name, Department, Number, Section, Professor, Year, Semester)
-VALUES ('8f8c4be1-a2d2-44b8-ba2e-06a4716c8578', 'Database Construction and Maintenance', 'CSCI', '2669', 'A', 'Robert Martin', 2014, 'FALL')
+VALUES ('8f8c4be1-a2d2-44b8-ba2e-06a4716c8578', 'Database Construction and Maintenance', 'CSCI', 2669, 'A', 'Robert Martin', 2014, 'FALL')
 
 INSERT INTO Courses (Id, Name, Department, Number, Section, Professor, Year, Semester)
-VALUES ('0f013aa9-7dcf-41a5-b1f5-4e785af734b3', 'Database Construction and Maintenance', 'CSCI', '2669', 'A', 'Robert Martin', 2013, 'FALL')
+VALUES ('0f013aa9-7dcf-41a5-b1f5-4e785af734b3', 'Database Construction and Maintenance', 'CSCI', 2669, 'A', 'Robert Martin', 2013, 'FALL')
 
 INSERT INTO Courses (Id, Name, Department, Number, Section, Professor, Year, Semester)
-VALUES ('2949CF66-3C9C-42A3-A820-4E53FD6BED3D', 'Database Construction and Maintenance', 'CSCI', '2669', 'B', 'Joel Spolsky', 2015, 'FALL')
+VALUES ('2949CF66-3C9C-42A3-A820-4E53FD6BED3D', 'Database Construction and Maintenance', 'CSCI', 2669, 'B', 'Joel Spolsky', 2015, 'FALL')
 
 INSERT INTO Courses (Id, Name, Department, Number, Professor, Year, Semester)
-VALUES ('28D64EBD-44BF-49E5-8709-49A6F291E3DF', 'Introduction to Model-View-Controller', 'CSCI', '1337', 'Scott Hanselmann', 2015, 'SPRING')
+VALUES ('28D64EBD-44BF-49E5-8709-49A6F291E3DF', 'Introduction to Model-View-Controller', 'CSCI', 1337, 'Scott Hanselmann', 2015, 'SPRING')
 
 INSERT INTO Courses (Id, Name, Department, Number, Professor, Year, Semester)
-VALUES ('E0936A90-9257-4CF5-B253-A05DF0B07E23', 'Magical Realism', 'ENGL', '3008', 'Jim Peterson', 2011, 'SPRING')
+VALUES ('E0936A90-9257-4CF5-B253-A05DF0B07E23', 'Magical Realism', 'ENGL', 3008, 'Jim Peterson', 2011, 'SPRING')
 
 INSERT INTO Courses (Id, Name, Department, Number, Professor, Year, Semester)
-VALUES ('D6A31382-D70F-407F-A58E-894F4B63344E', 'Science Fiction: Is It Just Fantasy With Steel Instead Of Trees?', 'ENGL', '4242', 'Orson Scott Card', 2014, 'SUMMER')
+VALUES ('D6A31382-D70F-407F-A58E-894F4B63344E', 'Science Fiction: Is It Just Fantasy With Steel Instead Of Trees?', 'ENGL', 4242, 'Orson Scott Card', 2014, 'SUMMER')
 
 INSERT INTO CoursesTaken (Student, Course, Grade)
 VALUES ('ABF60760-13EB-4BE5-A029-F1B7CF62B514', 'E0936A90-9257-4CF5-B253-A05DF0B07E23', 86)
---
+++ Tests/Domain/Repositories/SqlServerCourseTakenRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using CourseCentral.Domain.Models;
+using CourseCentral.Domain.Repositories;
+using Ninject;
+using NUnit.Framework;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace CourseCentral.Tests.Domain.Repositories
+{
+    [TestFixture]
+    public class SqlServerCourseTakenRepositoryTests : DatabaseTests
+    {
+        [Inject]
+        public CourseTakenRepository CourseTakenRepository { get; set; }
+        [Inject]
+        public StudentRepository StudentRepository { get; set; }
+        [Inject]
+        public CourseRepository CourseRepository { get; set; }
+        [Inject]
+        public Random Random { get; set; }
+
+        [Test]
+        public void FindCourseTaken()
+        {
+            var studentId = StudentRepository.FindAll().Last().Id;
+            var courseId = CourseRepository.FindAll().Last().Id;
+            var newCourseTaken = CreateCourseTaken(studentId, courseId);
+
+            var courseTaken = CourseTakenRepository.Find(newCourseTaken.Student, newCourseTaken.Course);
+            AssertCoursesTakenAreEqual(courseTaken, newCourseTaken);
+        }
+
+        [Test]
+        public void FindCoursesTakenByStudent()
+        {
+            var studentId = StudentRepository.FindAll().Last().Id;
+            var firstCourseId = CourseRepository.FindAll().First().Id;
+            var lastCourseId = CourseRepository.FindAll().Last().Id;
+            var firstNewCourseTaken = CreateCourseTaken(studentId, firstCourseId);
+            var secondNewCourseTaken = CreateCourseTaken(studentId, lastCourseId);
+
+            var coursesTaken = CourseTakenRepository.FindCourses(studentId);
+            Assert.That(coursesTaken, Is.Not.Empty);
+
+            var studentIds = coursesTaken.Select(c => c.Student);
+            Assert.That(studentIds, Is.All.EqualTo(studentId));
+
+            var firstCourseTaken = coursesTaken.First(c => c.Course == firstNewCourseTaken.Course);
+            AssertCoursesTakenAreEqual(firstCourseTaken, firstNewCourseTaken);
+
+            var secondCourseTaken = coursesTaken.First(c => c.Course == secondNewCourseTaken.Course);
+            AssertCoursesTakenAreEqual(secondCourseTaken, secondNewCourseTaken);
+        }
+
+        private void AssertCoursesTakenAreEqual(CourseTakenModel actual, CourseTakenModel expected)
+        {
+            Assert.That(actual.Course, Is.EqualTo(expected.Course));
+            Assert.That(actual.Student, Is.EqualTo(expected.Student));
+            Assert.That(actual.Grade, Is.EqualTo(expected.Grade));
+        }
+
+        private CourseTakenModel CreateCourseTaken(Guid student, Guid course)
+        {
+            var courseTaken = new CourseTakenModel
+            {
+                Student = student,
+                Course = course,
+                Grade = Random.Next(110)
+            };
+
+            var sql = @"
+                INSERT INTO CoursesTaken (Student, Course, Grade)
+                VALUES (@Student, @Course, @Grade)";
+
+            var parameters = new[]
+            {
+                new SqlParameter("@Student", courseTaken.Student),
+                new SqlParameter("@Course", courseTaken.Course),
+                new SqlParameter("@Grade", courseTaken.Grade)
+            };
+
+            using (var connection = GetAndOpenConnection())
+            using (var command = GetCommand(sql, connection, parameters))
+                command.ExecuteNonQuery();
+
+            return courseTaken;
+        }
+    }
+}
